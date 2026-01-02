96e1c26cc1aab81701372f4db7d4c3c76ef1a979
Samiran Banerjee <bsamibane786@gmail.com>
2026-01-03 01:30:41 +0530

    Add initial Java application and single row pattern implementation

---
+++ Learn-DSA-Java/Learn-DSA-Java/src/singleRowPattern.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class singleRowPattern {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your desired number :");
+        int n = sc.nextInt();
+
+        for (int i=0; i<n; i++){
+            System.out.print("* ");
+        }
+        sc.close();
+    }
+    
+}
