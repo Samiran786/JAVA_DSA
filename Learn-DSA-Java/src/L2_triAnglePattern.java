0d067db891b9473e48248685e9a2fe0e7b9277ac
Samiran Banerjee <bsamibane786@gmail.com>
2026-01-03 03:45:02 +0530

    Pattern 2 - Right angle pattern with star

---
+++ Learn-DSA-Java/src/L2_triAnglePattern.java
@@ -0,0 +1,33 @@
+/*
+    Problem: Print a triangle of stars - 
+        *
+        * *
+        * * *
+        * * * *
+ */
+
+import java.util.Scanner;
+
+public class L2_triAnglePattern {
+    public static void main(String [] args){
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+
+        int Nrow = sc.nextInt();
+        int Nstr = 1;
+
+        for (int i=0; i<Nrow; i++){
+            for (int j=0; j<Nstr; j++){
+                System.out.print("* ");
+            }
+            
+            Nstr++;
+            System.out.println();
+        }
+        sc.close();
+
+
+
+    }
+    
+}
