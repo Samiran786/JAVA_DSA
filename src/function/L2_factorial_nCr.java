29de892227dd435b562a9e5138bdee65f7c32def
Samiran Banerjee <bsamibane786@gmail.com>
2026-01-06 02:52:45 +0530

    Function 2 - Calculation of nCr

---
+++ src/function/L2_factorial_nCr.java
@@ -0,0 +1,32 @@
+package function;
+
+import java.util.Scanner;
+
+/*
+we have 2 value, n and r. find the combination with formula !n/(!r * !(n-r))
+ */
+public class L2_factorial_nCr {
+    public static void main(String [] args){
+        Scanner sc =  new Scanner(System.in);
+        System.out.print("Enter the number n : ");
+        int n = sc.nextInt();
+        System.out.print("Enter the number r : ");
+        int r = sc.nextInt();
+        int Nans=1;
+        int Rans=1;
+        for (int i=1; i<=n; i++){
+            Nans = Nans*i;
+        }
+        for(int i=1; i<=r; i++){
+            Rans = Rans*i;
+        }
+        int NRans=1;
+        for(int i=1; i<=(n-r); i++){
+            NRans = NRans*i;
+        }
+        int mainAns = Nans/(Rans*NRans);
+        System.out.print("Combination of the given numbers : "+mainAns);
+        sc.close();
+    }
+    
+}
